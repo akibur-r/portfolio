32c507d7cc69f13a5a7e67c549f023e94182e423
akibur-r <akibur.pciu@gmail.com>
2025-06-30 15:48:51 +0600

    Updated about me section
    Divided the bio in multiple paragraphs

--- src/components/homepage/about/About.tsx
+++ src/components/homepage/about/About.tsx
@@ -1,11 +1,15 @@
-import SectionHeading from "@/components/shared/sectionHeading/SectionHeading";
 import bio from "@/assets/docs/bio.json";
+import SectionHeading from "@/components/shared/sectionHeading/SectionHeading";
 
 const About = () => {
   return (
     <section className="text-sm text-neutral-700 dark:text-neutral-300">
       <SectionHeading name="About Me" />
-      <main>{bio.about_me_brief}</main>
+      <main className="space-y-1.5">
+        {bio.about_me_brief.map((para) => (
+          <p>{para}</p>
+        ))}
+      </main>
     </section>
   );
 };
